32d6c50c8d2a7a737553ee4e17ab215bba95e572
Nikolay <kolarulit@mail.ru>
2020-07-25 21:59:15 +0300

    stules and view index

---
+++ resources/views/electro/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.electro')
+
+@section('header')
+	@include('electro.sections.header')
+@endsection
+
+@section('content')
+	@include('electro.sections.contentIndex')
+@endsection
+
+@section('footer')
+	@include('electro.sections.footer')
+@endsection
