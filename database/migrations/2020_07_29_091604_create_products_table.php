ff3ee701144206d752e0481368b826e5003f3887
Nikolay <kolarulit@mail.ru>
2020-07-30 21:17:42 +0300

    first

--- database/migrations/2020_07_29_091604_create_products_table.php
+++ database/migrations/2020_07_29_091604_create_products_table.php
@@ -14,14 +14,14 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name', 50);
             $table->string('img', 50);
             $table->string('price', 120);
             $table->enum('new', ['yes', 'no']);
             $table->char('discount', 4);
             $table->string('pre_price', 50);
-            $table->integer('categories_id')->unsigned();
+            $table->bigInteger('categories_id')->unsigned();
             $table->foreign('categories_id')->references('id')->on('navigations');
             $table->string('cuantity', 20);
             $table->string('raiting', 5);
